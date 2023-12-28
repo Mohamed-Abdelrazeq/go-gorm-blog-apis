25efddb6e74bbd179b73ed3d069586bb9565bc54
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-12-28 08:40:01 +0200

    init go server

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		hostname, _ := os.Hostname()
+		fmt.Fprintf(w, "Hello from %s", hostname)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
